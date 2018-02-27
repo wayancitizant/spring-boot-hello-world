e20bb1a28c6eb27a82fb5734381912330d108f05
Barrett <ronald.barrett@citizant.com>
2018-02-27 10:29:50 -0500

    initial commit

---
+++ spring-boot-hello-world/src/main/java/sample/HelloWorldController.java
@@ -0,0 +1,40 @@
+package sample;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.togglz.core.Feature;
+import org.togglz.core.manager.FeatureManager;
+import org.togglz.core.util.NamedFeature;
+
+import java.util.Date;
+
+@RestController
+public class HelloWorldController {
+
+    private static final String GREETING = "Greetings from Spring Boot!";
+
+    private static final String DATE_TIME = new Date().toString();
+    
+    private final FeatureManager manager;
+
+    public static final Feature HELLO_WORLD = new NamedFeature("HELLO_WORLD");
+    
+    public static final Feature REVERSE_GREETING = new NamedFeature("REVERSE_GREETING");
+    
+    public HelloWorldController(FeatureManager manager) {
+		this.manager = manager;
+	}
+
+	@RequestMapping("/")
+    public ResponseEntity<?> index() {
+        if (manager.isActive(HELLO_WORLD)) {
+            StringBuilder sb = new StringBuilder(GREETING);
+            if (manager.isActive(REVERSE_GREETING)) {
+                sb.reverse();
+            }
+            return ResponseEntity.ok().body(sb.toString());
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
